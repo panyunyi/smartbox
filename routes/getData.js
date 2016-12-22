5b1ce76fd64663ca26afdc1671a7b17ea07dff37
panyunyi <panyunyi@126.com>
2016-12-22 14:09:10 +0800

    优化employee

--- routes/getData.js
+++ routes/getData.js
@@ -52,13 +52,21 @@ function doWork(cus,box,res,ts){
         query.equalTo('cusId',cus);
         query.find().then(function (results) {
             var arr=[];
-            results.forEach(function(result){
-                var cardQuery=new AV.Query('EmoployeeCard');
-                var one={"empNo":result.get('empNo'),"isDel":result.get('isDel'),"card":result.get('card'),"power":result.get('power'),"objectId":result.get('id'),"createdAt":result.get('createdAt'),"updatedAt":result.get('updatedAt')};
-                arr.push(one);
-            });
-            data["Employee"]=arr;
-            return callback(null,results);
+            async.map(results,function(result,callback2){
+                async.map(result.get('card'),function(card,callback1){
+                      var cardQuery = AV.Object.createWithoutData('EmployeeCard', card);
+                      cardQuery.fetch().then(function () {
+                        callback1(null,cardQuery.get('card'));
+                    });
+                },function(error,cards){
+                  var one={"empNo":result.get('empNo'),"isDel":result.get('isDel'),"card":cards,"power":result.get('power'),"objectId":result.get('id'),"createdAt":result.get('createdAt'),"updatedAt":result.get('updatedAt')};
+                  arr.push(one);
+                  callback2(null,one);
+                });
+            },function(error,array){
+              data["Employee"]=array;
+              return callback(null,results);
+          });
           }, function (error) {
             // 异常处理
             return callback(error)
@@ -105,26 +113,6 @@ function doWork(cus,box,res,ts){
             return callback(error)
           });
     }
-    function promise6(callback){
-        var query=new AV.Query('EmployeeCard');
-        query.greaterThanOrEqualTo('updatedAt',ts);
-        if(new Date(0)==ts){
-            query.equalTo('isDel',false);
-        }
-        query.equalTo('cusId',cus);
-        query.find().then(function (results) {
-            var arr=[];
-            results.forEach(function(result){
-                var one={"card":result.get('card'),"isDel":result.get('isDel'),"objectId":result.get('id'),"createdAt":result.get('createdAt'),"updatedAt":result.get('updatedAt')};
-                arr.push(one);
-            });
-            data["EmpCard"]=arr;
-            return callback(null,results);
-          }, function (error) {
-            // 异常处理
-            return callback(error)
-          });
-    }
     async.parallel([
         function (callback){
             promise1(callback);
@@ -140,9 +128,6 @@ function doWork(cus,box,res,ts){
         },
         function (callback){
             promise5(callback);
-        },
-        function(callback){
-            promise6(callback);
         }],function(err,results){
         var result={
             status:200,
