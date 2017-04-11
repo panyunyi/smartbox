851067cd8064d1d826b09cb5f79267f171825817
panyunyi <panyunyi@126.com>
2017-04-11 23:34:48 +0800

    修改

--- cloud.js
+++ cloud.js
@@ -1,3 +1,4 @@
+'use strict';
 var AV = require('leanengine');
 var async=require('async');
 var moment=require('moment');
@@ -5,27 +6,30 @@ var moment=require('moment');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('heartbeat', function(request, response) {
-    function promise1(callback){
-        var boxQuery=new AV.Query('BoxInfo');
+    /*function promise1(callback){
+        let boxQuery=new AV.Query('BoxInfo');
         boxQuery.equalTo('isDel',false);
         boxQuery.find().then(function(results){
             callback(null,results);
         });
     }
     function promise2(arg1,callback){
-        var query=new AV.Query('Todo');
-        query.descending('createdAt');
+        let query=new AV.Query('Todo');
+        //query.descending('createdAt');
         async.map(arg1,function(box,callback1){
+            query.descending('createdAt');
             query.equalTo('deviceId',box.get('deviceId'));
             query.first().then(function(data){
-                var result={};
-                var flag=true;
+                let result={};
+                let flag=true;
                 result['box']=box.get('deviceId');
                 if(typeof(data)=="undefined"){
                     flag=false;
                 }else{
                     var now=new Date();
+                    console.log(now+" "+data.get('createdAt'));
                     var time=now-data.get('createdAt');
+                    console.log(time);
                     if(time>5000){
                         flag=false;
                     }
@@ -52,8 +56,8 @@ AV.Cloud.define('heartbeat', function(request, response) {
         function (arg1,callback){
             promise2(arg1,callback);
         }],function(err,results){
-            response.success(results);
-    });
+            return response.success(results);
+    });*/
 });
 
 module.exports = AV.Cloud;
